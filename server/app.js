3d5d7e04c9ab9a857f95a9a3ea31e5fff4d078aa
Andrew Gunst <agunst99@gmail.com>
2018-01-30 08:56:03 -0500

    module 12 success

--- server/app.js
+++ server/app.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
@@ -9,6 +10,7 @@ User.sync(); // sync( {force: true}), to drop then create each time the app star
 
 app.use(bodyParser.json());
 app.use(require('./middleware/headers'));
+app.use(require('./middleware/validate-session'));
 //login route
 app.use('/api/login', require('./routes/session'));
 app.use('/api/user', require('./routes/user'));
