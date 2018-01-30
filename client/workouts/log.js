c0ba0259b5da254877775227e3c0ca2d8854eda0
Andrew Gunst <agunst99@gmail.com>
2018-01-30 17:11:00 -0500

    Update

--- client/workouts/log.js
+++ client/workouts/log.js
@@ -10,8 +10,11 @@ $(function() {
 				for (var i = 0; i < len; i++) {
 					opts += "<option value='" + defs[i].id +"'>" + defs[i].description + "</option>";
 				}
-				$("#log-definition").children().remove();
-				$("#log-definition").append(opts);
+                
+                $("#log-definition").children().remove();
+                $("#log-definition").append(opts);
+                $("#update-definition").children().remove();
+                $("#update-definition").append(opts)
 			},
 
 			setHistory: function() {
