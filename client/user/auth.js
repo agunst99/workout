4918b86d78ceb518423f44f1a5856ad71bc929b3
Andrew Gunst <agunst99@gmail.com>
2018-01-30 11:20:33 -0500

    module 16 Success

--- client/user/auth.js
+++ client/user/auth.js
@@ -7,26 +7,28 @@ $(function() {
              var signup = $.ajax({
                 type: "POST", 
                 url: WorkoutLog.API_BASE + "user", 
-                data: JSON.stringify( user ), 
+                data: JSON.stringify(user), 
                 contentType: "application/json"
              });
-             signup.done(function(data) {
+             signup
+             .done(function(data) {
                 if (data.sessionToken) {
                    WorkoutLog.setAuthHeader(data.sessionToken);
                    console.log("You made it!");
                    console.log(data.sessionToken);
-                   
                 }
                 $("#signup-modal").modal("hide");
                 $(".disabled").removeClass("disabled");
-                $("#loginout").text("Logout");
-                // go to define tab
-                $('.nav-tabs a[href="#define"]').tab('show');
+                //$("#loginout").text("Logout");
+                //go to define tab
+                //$('.nav-tabs a[href="#define"]').tab('show');
              })
-             .fail(function() {
+             .fail(function(){
                 $("#su_error").text("There was an issue with your username").show();
              });
        },
+ 
+       //login
  
        login: function() {
             var username = $("#li_username").val();
@@ -38,21 +40,26 @@ $(function() {
                 data: JSON.stringify(user), 
                 contentType: "application/json"
             });
-            login.done(function(data) {
+ 
+            login
+            .done(function(data) {
                 if (data.sessionToken) {
-                WorkoutLog.setAuthHeader(data.sessionToken);
-                
+                WorkoutLog.setAuthHeader(data.sessionToken);       
+                console.log(data.sessionToken);
                 }
-                // TODO: add logic to set user and auth token	
+ 
                 $("#login-modal").modal("hide");
                 $(".disabled").removeClass("disabled");
                 $("#loginout").text("Logout");
             })
-            .fail(function() {
+ 
+  
+            .fail(function(){
                 $("#li_error").text("There was an issue with your username or password").show();
                });
        },
  
+       //logout
        loginout: function() {
           if (window.localStorage.getItem("sessionToken")) {
              window.localStorage.removeItem("sessionToken");
@@ -62,9 +69,9 @@ $(function() {
     });
  
     // bind events
-    $("#login").on("click", WorkoutLog.login);
-    $("#signup").on("click", WorkoutLog.signup);
-    $("#loginout").on("click", WorkoutLog.loginout);
+       $("#login").on("click", WorkoutLog.login);
+     $("#signup").on("click", WorkoutLog.signup);
+      $("#loginout").on("click", WorkoutLog.loginout);
  
     if (window.localStorage.getItem("sessionToken")) {
        $("#loginout").text("Logout");
